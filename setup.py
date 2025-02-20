420e3bd008456ebb9b04cbbe7894ef239313d699
Tim van den Akker <34026618+Tim-van-den-Akker@users.noreply.github.com>
2025-02-20 11:30:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 import setuptools
 from setuptools.command.install import install
 
-INSTALLVERSION="3.2.7"
+INSTALLVERSION="3.2.8"
 
 class InstallWrapper(install):
     """
